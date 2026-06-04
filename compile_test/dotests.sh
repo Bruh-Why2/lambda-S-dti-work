eda9daee5000b89ada79b1e685e036bd483908ac
oshimayuki1124 <yukioshima11124457@gmail.com>
2026-06-04 14:24:43 +0900

    add tests for issue2

--- compile_test/dotests.sh
+++ compile_test/dotests.sh
@@ -91,7 +91,8 @@ run_test "repeat.ml" "4"
 # --- issue 1 ---
 # run_test "anoter_name.ml" "skip_static"
 # --- issue 2 ---
-# run_test "dyn_empty_list.ml" "skip_static"
+run_test "issue2_example1.ml" "3" "skip_static"
+run_test "issue2_example2.ml" "$(printf "Blame on the expression side:\nFile \"issue2_example2.ml\", line 1, character 33 -- line 1, character 81")" "skip_static"
 
 
 # --- 判定結果 ---
